86872760999c5963c5c9dc26cca1a8ad6ed105d7
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2025-11-16 21:52:08 +0600

    problem 2 & 3 solved

--- solution.ts
+++ solution.ts
@@ -11,3 +11,25 @@ const formatValue = (value: FormatData): FormatData => {
 
 	return !value;
 };
+
+const getLength = (value: string | any[]): number => {
+	if (Array.isArray(value)) {
+		return value.length;
+	}
+
+	return value.length;
+};
+
+class Person {
+	name: string;
+	age: number;
+
+	constructor(name: string, age: number) {
+		this.name = name;
+		this.age = age;
+	}
+
+	getDetails(): string {
+		return `Name: ${this.name}, Age: ${this.age}`;
+	}
+}
