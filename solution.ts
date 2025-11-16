8c5ea8485dc4c8b19c7ddccc656086e5d47a30ea
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2025-11-16 22:47:16 +0600

    problem 4 & 5 solved

--- solution.ts
+++ solution.ts
@@ -33,3 +33,41 @@ class Person {
 		return `Name: ${this.name}, Age: ${this.age}`;
 	}
 }
+
+interface Item {
+	title: string;
+	rating: number;
+}
+
+const filterByRating = (items: Item[]): Item[] => {
+	return items.reduce<Item[]>((acc, item): Item[] => {
+		if (item.rating >= 4) {
+			acc.push(item);
+		}
+		return acc;
+	}, []);
+};
+
+interface User {
+	id: number;
+	name: string;
+	email: string;
+	isActive: boolean;
+}
+
+const filterActiveUsers = (users: User[]) => {
+	return users.reduce<User[]>((acc, user): User[] => {
+		if (user.isActive) {
+			acc.push(user);
+		}
+		return acc;
+	}, []);
+};
+
+const users = [
+	{ id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
+	{ id: 2, name: "Asha", email: "asha@example.com", isActive: false },
+	{ id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
+];
+
+console.log(filterActiveUsers(users));
